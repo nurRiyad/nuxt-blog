ecd5ccc3512b5bcb145cbc8ee495360850ea6b8a
nurRiyad <asadnurriyad@gmail.com>
2022-12-08 00:50:25 +0600

    add route
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- pages/[topics]/[post].vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1 class="text-5xl">
-      post name {{ $route.params.post }} of topic
-      {{ $route.params.topics }} will be here
-    </h1>
-  </div>
-</template>
--- pages/[topics]/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1 class="text-5xl">
-      All the post related to {{ $route.params.topics }} will be here
-    </h1>
-  </div>
-</template>
