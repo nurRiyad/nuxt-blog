fca51145adc920b894a573bd1cd4f9b0555937ba
nurRiyad <asadnurriyad@gmail.com>
2022-12-10 03:08:51 +0600

    update navbar
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <div class="h-10 bg-[#a2d9ff]"></div>
     <navbar-com />
     <div class="h-10 bg-[#a2d9ff]"></div>
-    <svg-wave />
+    <img src="@/assets/img/wave.png" class="w-full" alt="this is wave image" />
     <div>
       <slot />
     </div>
