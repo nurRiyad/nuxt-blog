4ad57c3c5f75a49f04272bd0505553e4382e697b
nurRiyad <asadnurriyad@gmail.com>
2022-12-10 04:04:51 +0600

    add basic body
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

---
+++ components/BlogCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="space-y-2">
+    <h3 class="text-2xl text-slate-800 font-semibold">Color formate in CSS</h3>
+    <p class="pr-5 text-slate-700">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea rem, maiores
+      fugit adipisci quis iusto tempora veniam vitae explicabo corporis
+      accusantium facilis iste, libero impedit dolorem perspiciatis dignissimos
+      cumque reprehenderit!
+    </p>
+    <p class="font-semibold">Read more</p>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,23 @@ let x = ref(10);
 </script>
 
 <template>
-  <div class="container px-3 mx-auto max-w-6xl flex">
-    <div>
-      <h1>This is main section</h1>
+  <div
+    class="container px-5 mx-auto max-w-6xl flex font-poppins space-x-5 antialiased"
+  >
+    <div class="flex-1">
+      <h1 class="text-xl pb-8 tracking-wide text-[#e60067]">
+        RECENTLY PUBLISHED
+      </h1>
+      <div class="space-y-8">
+        <blog-card v-for="n in 10" :key="n" />
+      </div>
     </div>
-    <div>
-      <h1>This is sub section</h1>
+    <div class="max-w-[300px]">
+      <div>
+        <h2 class="text-xl pb-8 tracking-wide text-[#e60067]">
+          TOP CATEGORIES
+        </h2>
+      </div>
     </div>
   </div>
 </template>
