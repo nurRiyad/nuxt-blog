b6bc24411a0a3a51c19452d254031dd2880d0a0c
nurRiyad <asadnurriyad@gmail.com>
2022-12-19 02:25:29 +0600

    update color
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="space-y-2 my-5 group">
-    <h3 class="text-xl text-slate-800 font-semibold group-hover:text-sky-500">
+  <div class="space-y-2 my-5 text-slate-800 group">
+    <h3 class="text-xl font-semibold group-hover:text-sky-500">
       This is a blog title
     </h3>
-    <p class="text-slate-700">
+    <p class="text-slate-600">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, deserunt
       esse tenetur iusto dolor unde. Aliquam quidem ab qui velit?
     </p>
