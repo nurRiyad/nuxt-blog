7949eaa67ab8a677e53583357f227261cd646523
nurRiyad <asadnurriyad@gmail.com>
2022-12-22 01:57:55 +0600

    add page transition
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- app.vue
+++ app.vue
@@ -6,3 +6,14 @@
     </NuxtLayout>
   </div>
 </template>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
   typescript: {
     strict: true,
   },
