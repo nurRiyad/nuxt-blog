bb2c026901607097801f41ce84535f77703ba7b6
nurRiyad <asadnurriyad@gmail.com>
2023-01-05 22:02:43 +0600

    update content render
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- app.vue
+++ app.vue
@@ -23,31 +23,33 @@ useState("blogData", () => (data.value as Array<ParsedContent>) || []);
 <template>
   <div>
     <NuxtLayout>
-      <NuxtLoadingIndicator />
+      <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
 <style>
-/* Page transition */
-.page-enter-active,
+.page-enter-active {
+  transition: all 0.1s ease-out;
+}
 .page-leave-active {
-  transition: all 0.2s;
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
 }
 .page-enter-from,
 .page-leave-to {
+  transform: translateY(20px);
   opacity: 0;
-  filter: blur(1rem);
 }
 
-/* Layout Transition */
-.layout-enter-active,
+.layout-enter-active {
+  transition: all 0.1s ease-out;
+}
 .layout-leave-active {
-  transition: all 0.3s;
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
 }
 .layout-enter-from,
 .layout-leave-to {
+  transform: translateY(-20px);
   opacity: 0;
-  filter: blur(1rem);
 }
 </style>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,21 +1,6 @@
 <script setup lang="ts">
-import { ParsedContent } from "@nuxt/content/dist/runtime/types";
-
 definePageMeta({
   layout: "blog",
-});
-
-const route = useRoute();
-
-const path = computed(() => {
-  return route.path || "";
-});
-
-const data = useState("blogData");
-
-const content = computed(() => {
-  const allpost = (data.value as Array<ParsedContent>) || [];
-  return allpost.find((post) => post._path === path.value) || {};
 });
 </script>
 
@@ -23,8 +8,6 @@ const content = computed(() => {
   <main
     class="container mx-auto bg-white max-w-6xl p-6 min-h-screen prose prose-slate"
   >
-    <ContentRenderer :value="content">
-      <ContentRendererMarkdown :value="content" />
-    </ContentRenderer>
+    <ContentDoc />
   </main>
 </template>
