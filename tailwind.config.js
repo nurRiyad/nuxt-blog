fddfe6bb3e3ee03f32418e767f1a90c5ab05c90b
nurRiyad <asadnurriyad@gmail.com>
2023-01-07 02:18:50 +0600

    update
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// remember to fix this issue wint the tailwind/nuxt
+// https://github.com/tailwindlabs/tailwindcss-intellisense/issues/663#issuecomment-1316788128
+
 module.exports = {
   content: [],
   theme: {
