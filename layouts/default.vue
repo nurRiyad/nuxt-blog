370e5f4fc7daef94f305747fc9b5360249806633
nurRiyad <asadnurriyad@gmail.com>
2023-01-30 00:43:21 +0600

    remove transparent navbar make it white
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="gd-container font-spacegrotesk">
-    <header class="fixed w-full backdrop-blur-3xl z-10">
+    <header class="fixed w-full bg-white z-10">
       <MainHeader />
     </header>
     <main>
