3852bcc76ce5bfa8c3abe5a2f65a6575298d0787
nurRiyad <asadnurriyad@gmail.com>
2023-08-05 13:40:31 +0600

    Fix mobile responsive design issue
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- pages/blogs/[blog].vue
+++ pages/blogs/[blog].vue
@@ -74,7 +74,7 @@ useHead({
 </script>
 
 <template>
-  <div class="px-6 container max-w-5xl mx-auto grid grid-cols-12 gap-x-12 ">
+  <div class="px-6 container max-w-5xl mx-auto sm:grid grid-cols-12 gap-x-12 ">
     <div class="col-span-12 lg:col-span-9">
       <BlogHeader
         :title="data.title"
