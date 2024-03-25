193d033867f9ed2d84332d9d5aa6946e959216b3
Maxime Deroullers <maxime.deroullers@gmail.com>
2024-03-25 22:20:35 +0100

    style: fix dark title css

--- components/main/trending.vue
+++ components/main/trending.vue
@@ -36,24 +36,15 @@ useHead({
 <template>
   <div class="px-4">
     <div class="flex flex-row items-center space-x-3 pt-5 pb-3">
-      <Icon name="mdi:star-three-points-outline" size="2em" class="text-black dark:text-zinc-300  " />
-      <h2 class="text-4xl font-semibold text-black dark:text-zinc-3000 font-orbitron ">
+      <Icon name="mdi:star-three-points-outline" size="2em" class="text-black dark:text-zinc-300" />
+      <h2 class="text-4xl font-semibold text-black dark:text-zinc-3000 font-orbitron dark:text-zinc-300">
         Articles Populaires
       </h2>
     </div>
     <div class="grid grid-cols-1 ">
       <template v-for="post in formattedData" :key="post.title">
-        <ArchiveCard
-          :path="post.path"
-          :title="post.title"
-          :date="post.date"
-          :description="post.description"
-          :image="post.image"
-          :alt="post.alt"
-          :og-image="post.ogImage"
-          :tags="post.tags"
-          :published="post.published"
-        />
+        <ArchiveCard :path="post.path" :title="post.title" :date="post.date" :description="post.description"
+          :image="post.image" :alt="post.alt" :og-image="post.ogImage" :tags="post.tags" :published="post.published"/>
       </template>
       <template v-if="data?.length === 0">
         <BlogEmpty />
