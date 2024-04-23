b1293c22db7689b2322f11cc10f7a3acdb9022ab
Maxime Deroullers <maxime.deroullers@gmail.com>
2024-04-23 10:55:57 +0200

    style: change svg for no article available

--- components/blog/empty.vue
+++ components/blog/empty.vue
@@ -8,11 +8,11 @@
       </div>
       <div class="p-5">
         <h2 class="text-3xl font-semibold text-black dark:text-zinc-300 pb-1 group-hover:text-sky-700 dark:group-hover:text-sky-400">
-          No Post Available
+          Aucun article disponible
         </h2>
 
-        <div class="flex group-hover:underline text-sky-700 dark:text-sky-400 items-center pt-2">
-          <p>Back To Home</p>
+        <div class="flex group-hover:underline text-sky-700 dark:text-sky-4000 items-center pt-2">
+          <p>Retourner à l'accueil</p>
           <LogoArrow />
         </div>
       </div>
