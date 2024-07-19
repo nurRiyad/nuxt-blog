f01fb38e0ee65e0ecff92c7db70ae7fc7bbb09a8
Maxime Deroullers <maxime.deroullers@gmail.com>
2024-07-19 16:48:21 +0200

    clean code notion services

--- server/services/notionService.ts
+++ server/services/notionService.ts
@@ -4,19 +4,17 @@ import type { NotionBlock, NotionPage } from '../../types/notion'
 
 const notion = new Client({ auth: NOTION_API_KEY })
 
-export async function fetchPostsToPublishFromNotion() {
+export async function fetchPostsToPublishFromNotion(): Promise<any[]> {
   try {
     const response = await notion.databases.query({
       database_id: DATABASE_POSTS_ID,
       filter: {
         property: 'Status',
-        status: {
-          equals: 'To Publish',
-        },
+        status: { equals: 'To Publish' },
       },
     })
 
-    return Promise.all(response.results.map(result => getPageContent(result as NotionPage)))
+    return Promise.all(response.results.map((result: any) => getPageContent(result as NotionPage)))
   }
   catch (error) {
     console.error('Erreur lors de la récupération des articles :', error)
@@ -24,7 +22,7 @@ export async function fetchPostsToPublishFromNotion() {
   }
 }
 
-async function getPageContent(page: NotionPage) {
+async function getPageContent(page: NotionPage): Promise<{ id: string; title: string; content: string }> {
   try {
     const blocks = await notion.blocks.children.list({ block_id: page.id })
     const markdownContent = convertBlocksToMarkdown(blocks.results as NotionBlock[])
@@ -41,45 +39,36 @@ async function getPageContent(page: NotionPage) {
   }
 }
 
-// function extractContentFromBlocks(blocks: NotionBlock[]) {
-//   return blocks
-//     .map((block) => {
-//       if (block.type === 'paragraph' && block.paragraph)
-//         return block.paragraph.rich_text.map(text => text.plain_text).join('')
-//       return ''
-//     })
-//     .join('\n\n')
-// }
-
 function convertBlocksToMarkdown(blocks: NotionBlock[]): string {
-  return blocks.map((block) => {
-    switch (block.type) {
-      case 'paragraph':
-        return `${block.paragraph?.rich_text.map(text => text.plain_text).join('')}\n\n`
-      case 'heading_1':
-        return `# ${block.heading_1?.rich_text.map(text => text.plain_text).join('')}\n\n`
-      case 'heading_2':
-        return `## ${block.heading_2?.rich_text.map(text => text.plain_text).join('')}\n\n`
-      case 'heading_3':
-        return `### ${block.heading_3?.rich_text.map(text => text.plain_text).join('')}\n\n`
-      case 'bulleted_list_item':
-        return `- ${block.bulleted_list_item?.rich_text.map(text => text.plain_text).join('')}\n`
-      case 'numbered_list_item':
-        return `1. ${block.numbered_list_item?.rich_text.map(text => text.plain_text).join('')}\n`
-      case 'code':
-        return `\`\`\`${block.code?.language || ''}
-${block.code?.rich_text.map((text: any) => text.plain_text).join('')}
-\`\`\`\n\n`
-      case 'image': {
-        const imageUrl = block.image?.file?.url || block.image?.external?.url || ''
-        return `![Image](${imageUrl})\n\n`
-      }
-      default:
-        return ''
-    }
-  }).join('')
+  const blockToMarkdown: { [key: string]: (block: any) => string } = {
+    paragraph: (block: any) => `${extractText(block.paragraph)}\n\n`,
+    heading_1: (block: any) => `# ${extractText(block.heading_1)}\n\n`,
+    heading_2: (block: any) => `## ${extractText(block.heading_2)}\n\n`,
+    heading_3: (block: any) => `### ${extractText(block.heading_3)}\n\n`,
+    bulleted_list_item: (block: any) => `- ${extractText(block.bulleted_list_item)}\n`,
+    numbered_list_item: (block: any) => `1. ${extractText(block.numbered_list_item)}\n`,
+    code: (block: any) => codeToMarkdown(block),
+    image: (block: any) => imageToMarkdown(block),
+  }
+
+  return blocks.map(block => blockToMarkdown[block.type]?.(block) || '').join('')
 }
 
-function extractTitleFromPage(page: NotionPage) {
+function extractText(blockContent: any): string {
+  return blockContent?.rich_text.map((text: any) => text.plain_text).join('')
+}
+
+function codeToMarkdown(block: any): string {
+  const language = block.code?.language || ''
+  const code = extractText(block.code)
+  return `\`\`\`${language}\n${code}\n\`\`\`\n\n`
+}
+
+function imageToMarkdown(block: any): string {
+  const imageUrl = block.image?.file?.url || block.image?.external?.url || ''
+  return `![Image](${imageUrl})\n\n`
+}
+
+function extractTitleFromPage(page: NotionPage): string {
   return page.properties?.Name?.title[0]?.plain_text || 'Sans titre'
 }
