d3b9708a4cec2eb0a6bb50f00abed8ceb0ac43bd
nurRiyad <nur.riyad@fieldnation.com>
2025-03-12 03:45:43 +0600

    Fix formatting in content.config.ts

--- content.config.ts
+++ content.config.ts
@@ -4,7 +4,7 @@ export default defineContentConfig({
   collections: {
     content: defineCollection({
       type: 'page',
-      source: '**/*.md'
-    })
-  }
+      source: '**/*.md',
+    }),
+  },
 })
